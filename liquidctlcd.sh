2c124a94f39bfb74b7d29efd58db7bc8bc6d9520
Me1k0r <runner-me1k0r@proton.me>
2025-07-16 10:02:03 +0200

    edit: liquidctlcd - .quad only when not gif

--- liquidctlcd.sh
+++ liquidctlcd.sh
@@ -3,12 +3,12 @@
 scrDir="$(dirname "$(realpath "$0")")"
 source "${scrDir}/globalcontrol.sh"
 
-currWpp=$(readlink "${cacheDir}/wall.sqre")
-mode="static"
+currWpp=$(readlink "${cacheDir}/wall.set")
+mode="gif"
 
 if [[ $currWpp == *"gif"* ]] ; then
-  $currWpp=$(readlink "${cacheDir}/wall.set")
-  mode="gif"
+  $currWpp=$(readlink "${cacheDir}/wall.quad")
+  mode="static"
 fi
 
 liquidctl --match kraken set lcd screen $mode "$currWpp"
